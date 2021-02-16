2588c49e4d9897402ce629ef758b23cfcc811656
SanazGheibi <sgheibi@ufl.edu>
2021-02-15 20:48:46 -0500

    works correctly on a toy example

--- data/airportFreqAnalysis.cpp
+++ data/airportFreqAnalysis.cpp
@@ -16,11 +16,12 @@ using namespace std;
 
 /*This version of Edge doesn't have "t" info*/
 struct Edge{
-    int u, v, w; 
+    int u, v, t, w; 
     Edge(){}
-    Edge(int _u, int _v, int _w){
+    Edge(int _u, int _v, int _t, int _w){
 	u = _u; 
 	v = _v;
+	t = _t;
 	w = _w; 
     }
 };  
@@ -32,12 +33,18 @@ struct mycomp {
 	if(e1.u != e2.u) return e1.u < e2.u;
 	if(e1.v != e2.v) return e1.v < e2.v;
 	if(e1.w != e2.w) return e1.w < e2.w;
+	return e1.t < e2.t;
   }
-};
+}myCmp;
 
+bool operator == (Edge e1, Edge e2){
+	if(e1.u == e2.u && e1.v == e2.v && e1.w == e2.w)
+		return true;
+	return false;
+}
 
 /*for dealing with the frequencies*/
-map<Edge, vector<int>, mycomp> freqMap;
+vector<Edge> edge_list;
 
 int main(int argc, char* argv[]){
     if(argc < 2){
@@ -51,19 +58,34 @@ int main(int argc, char* argv[]){
 	return 0;
     }
 
-    int V, numEdges; 
-    int t; 
+    int V, numEdges;
     fscanf(inFile, "%d %d", &V, &numEdges);
     for(int i = 0; i < numEdges; i++)
     {
     	Edge e;
-        if(fscanf(inFile, "%d %d %d %d",&e.u, &e.v, &t, &e.w) == EOF){
+        if(fscanf(inFile, "%d %d %d %d",&e.u, &e.v, &e.t, &e.w) == EOF){
 	   cout << "error in reading the file" << endl;
 	   return 0; 
 	}
-        freqMap[e].push_back(t); //we assume edges are sorted by t
+        edge_list.push_back(e); //we assume edges are sorted by t
     }   
     fclose(inFile);
+
+    sort(edge_list.begin(), edge_list.end(), myCmp);
+      
+    vector<vector<int>> freqList;
+    vector<Edge>::iterator cur = edge_list.begin();
+    vector<Edge>::iterator prev;
+    for(; cur != edge_list.end(); cur++){
+	if(cur != edge_list.begin() && *cur == *prev)
+	   freqList[freqList.size()-1].push_back(cur->t);
+        else{
+	   vector<int> tmp;
+	   tmp.push_back(cur->t);
+	   freqList.push_back(tmp);
+	} 
+	prev = cur;
+    }
 
     /*two sets used to take the intersection of all \delta_t values*/
     unordered_set<int> firstSet;
@@ -71,22 +93,22 @@ int main(int argc, char* argv[]){
 
     int tIndx;
     int deltaT;
-    map<Edge, vector<int>>::iterator it = freqMap.begin();
-    vector<int> vTmp = it->second;
+    vector<vector<int>>::iterator it = freqList.begin();
+    vector<int> vTmp = *it;
     for(tIndx=1; tIndx < vTmp.size(); tIndx++){
-	deltaT = vTmp[tIndx]-vTmp[tIndx-1];
+	deltaT = vTmp[tIndx]-vTmp[0];
 	firstSet.insert(deltaT);
     }
     it++;
 
-    for(; it != freqMap.end(); it++){
+    for(; it != freqList.end(); it++){
 	if(firstSet.empty()){
 	   cout << "no common period" << endl;
 	   return 0;
 	}
-	vTmp = it->second;
+	vTmp = *it;
         for(tIndx=1; tIndx < vTmp.size(); tIndx++){
-	    deltaT = vTmp[tIndx]-vTmp[tIndx-1];
+	    deltaT = vTmp[tIndx]-vTmp[0];
 	    if(firstSet.find(deltaT) != firstSet.end())
 	       secondSet.insert(deltaT);
         }
