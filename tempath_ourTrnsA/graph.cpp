f3b57c4530eb53683fc687ce9328cd4f48a4b251
SanazGheibi <sgheibi@ufl.edu>
2021-02-20 21:49:16 -0500

    Transformation A files added - in progress

---
+++ tempath_ourTrnsA/graph.cpp
@@ -0,0 +1,407 @@
+#include "graph.h"
+
+Graph::Graph(const char* filePath)
+{
+    FILE* file = fopen(filePath,"r");
+	int x;
+   	x=fscanf(file, "%d %d",&V, &dynamic_E);
+
+	//arr_time.resize(V);
+	//f_time.resize(V);
+	//ft_timepair.resize(V);
+	//st_timepair.resize(V);
+    
+    //added by sanaz:
+    Vin.resize(V);
+    Vout.resize(V);
+    distances.resize(V); 
+    //---------------
+	
+	Edge e; 
+    for(int i = 0; i < dynamic_E; i ++)
+    {
+        x=fscanf(file, "%d %d %d %d",&e.u, &e.v, &e.t, &e.w);
+        edge_list.push_back(e);
+    }
+
+    fclose(file);
+}
+
+//added by sanaz
+void Graph::transform(){
+   vector<set<int>> Tin; //the set of distinct in times for each node
+   //vector<set<int>> Tout; //the set of distinct out times for each node
+   Tin.resize(V);
+   //Tout.resize(V); 
+   for(Edge e : edge_list){
+	//initially set all the newIDs (indices in the transformed graph) to -1
+	Tin[e.v].insert(e.t+e.w);
+	//Tout[e.u].insert(e.t);
+   }
+
+   set <int>::iterator it; 
+   //two maps to map (u, t) in Tin/Tout to their corresponding IDs in the transformed graph
+   map<pair<int, int>, int> inMap;
+   //map<pair<int, int>, int> outMap;
+   int index = 0; 
+   //Node tmpNode; 
+   int t; 
+   for(int i=0; i<V; i++){
+	for(it=Tin[i].begin(); it!=Tin[i].end(); it++){
+	  t = *it;
+	  //tmpNode = new Node(i, t, true);
+          Node tmpNode(i, t, true); 
+	  node_list.push_back(tmpNode);
+	  Vin[i].insert(index);
+	  inMap[make_pair(i, t)] = index++;
+	}
+	//for(it=Tout[i].begin(); it!=Tout[i].end(); it++){
+	  //t = *it;
+	  //tmpNode = new Node(i, t, false);
+	  //Node tmpNode(i, t, false); 
+	  //node_list.push_back(tmpNode);
+	  //Vout[i].insert(index);
+	  //outMap[make_pair(i, t)] = index++;
+	//}
+   }
+   adj_list.resize(index); 
+
+   //edge creation step:
+   for(Edge e : edge_list){
+	int v_index = inMap[make_pair(e.v, e.t+e.w)];
+	for(t : Tin[e.u])
+	   if(t <= e.t){
+	      int u_index = inMap[make_pair(e.u, e.t)];
+	      adj_list[u_index].push_back(make_pair(v_index, e.w));
+	   }
+   }
+
+   //filling up the reverse adjacency list:
+   rev_adjList.resize(index);
+   for(int i=0; i<index; i++){
+	for(auto neigh=adj_list[i].begin(); neigh!=adj_list[i].end(); neigh++){
+	    rev_adjList[neigh->first].push_back(i); 
+        }
+   }
+}
+void Graph::print_adjList(){  
+   cout << "(u1, t1, Vin1/Vout1) (u2, t2, Vin2/Vout2) W" << endl; 
+
+   for(int i=0; i<adj_list.size(); i++){
+
+     for(int j=0; j<adj_list[i].size(); j++){	
+
+        //print out the source node 
+	cout << "(" << node_list[i].u << ", " << node_list[i].t << ", "; 
+	if(node_list[i].isVin)
+	   cout << "Vin) ";
+	else 
+	   cout << "Vout) ";
+
+	//print out the destination node
+	int id = adj_list[i][j].first;
+	int w = adj_list[i][j].second; 
+        cout << "(" << node_list[id].u << ", " << node_list[id].t << ", ";
+	if(node_list[id].isVin)
+	   cout << "Vin) ";
+	else 
+	   cout << "Vout) ";
+	cout << w << endl; 
+     }
+   }
+}
+//--------------
+
+void Graph::initial_query(int numS)
+{
+    t_start = 0;
+    t_end = infinity;
+
+    /*added by sanaz*/
+    numSources = numS; 
+	
+    int s;
+    for(int i = 0 ; i < numSources; i++)
+    {
+    	s=rand()%V;
+        sources.push_back(s);
+    }
+
+}
+
+void Graph::initial_query(const char* filePath, int numS)
+{
+    t_start = 0;
+    t_end = infinity;
+
+    /*added by sanaz*/
+    numSources = numS; 
+	
+    FILE* file = fopen(filePath,"r");
+    int s, x;
+    for(int i = 0 ; i < numSources; i++)
+    {
+    	x=fscanf(file,"%d",&s);
+    //	int y;
+    //	x=fscanf(file,"%d%d",&s, &y);
+        sources.push_back(s);
+    }
+
+}
+
+void Graph::run_earliest_arrival()
+{
+	time_sum=0;
+	
+	for(int i = 0 ;i < sources.size(); i ++)
+    { 
+    	//modified by sanaz:
+	for(int j=0; j<V; j++)
+	  distances[j] = infinity; 
+	distances[sources[i]] = t_start; 
+	//------------------
+    	earliest_arrival(sources[i]);
+    }
+	
+	print_avg_time();
+}
+
+//modified by sanaz
+void Graph::earliest_arrival(int source)
+{
+    Timer t;
+    t.start();
+	
+    /*define and initialize data structures*/	
+    vector<bool> visited(node_list.size(), false);
+    queue<int> Q; 
+
+    /*initializing Q*/
+    set<int>::iterator it; 
+    for(it = Vout[source].begin(); it != Vout[source].end(); it++){
+	if(node_list[*it].t >= t_start && node_list[*it].t <= t_end){
+	   visited[*it] = true; //it->first: t, it->second: newID
+           Q.push(*it);
+        }
+    }
+
+    while(!Q.empty()){
+	int node = Q.front(); 
+	Q.pop();
+	for(auto neighbor=adj_list[node].begin(); neighbor!=adj_list[node].end(); neighbor++){
+	    int nID = neighbor->first; 
+	    Node neiNode = node_list[nID];
+	    if(!visited[nID] && neiNode.t >= t_start && neiNode.t <= t_end){
+		visited[nID] = true; 
+		Q.push(nID);
+		if(neiNode.isVin == true && neiNode.t < distances[neiNode.u])
+		   distances[neiNode.u] = neiNode.t; 
+	    }
+	}
+    }
+	
+    t.stop();
+    time_sum += t.GetRuntime();
+
+    /*for debugging only*/
+    for(int i=0; i<distances.size(); i++)
+	cout << distances[i] << endl; 
+
+}
+//-----------------
+
+void Graph::run_latest_departure()
+{
+	time_sum=0;
+	
+	for(int i = 0 ;i < sources.size() ;i ++)
+    { 
+    	//modified by sanaz:
+	for(int j=0; j<V; j++)
+	  distances[j] = t_start-1; 
+	distances[sources[i]] = t_end; 
+	//------------------
+    	latest_departure(sources[i]);
+    }
+	
+	print_avg_time();
+}
+
+void Graph::latest_departure(int source)
+{
+    Timer t;
+    t.start();
+	
+    /*define and initialize data structures*/	
+    vector<bool> visited(node_list.size(), false);
+    queue<int> Q; 
+
+    /*initializing Q*/
+    set<int>::iterator it; 
+    for(it = Vin[source].begin(); it != Vin[source].end(); it++){
+	if(node_list[*it].t >= t_start && node_list[*it].t <= t_end){
+	   visited[*it] = true; //it points to the index of the node
+           Q.push(*it);
+        }
+    }
+
+    while(!Q.empty()){
+	int node = Q.front(); 
+	Q.pop();
+	for(auto neighbor=rev_adjList[node].begin(); neighbor!=rev_adjList[node].end(); neighbor++){
+	    int nID = *neighbor; 
+	    Node neiNode = node_list[nID];
+	    if(!visited[nID] && neiNode.t >= t_start && neiNode.t <= t_end){
+		visited[nID] = true; 
+		Q.push(nID);
+		if(neiNode.isVin == false && neiNode.t > distances[neiNode.u])
+		   distances[neiNode.u] = neiNode.t; 
+	    }
+	}
+    }
+	
+    t.stop();
+    time_sum += t.GetRuntime();
+
+    /*for debugging only*/
+    for(int i=0; i<distances.size(); i++)
+	cout << distances[i] << endl; 
+
+}
+
+void Graph::run_fastest()
+{
+	time_sum=0;
+	
+	for(int i = 0 ;i < sources.size() ;i ++)
+    { 
+    	//modified by sanaz:
+	for(int j=0; j<V; j++)
+	  distances[j] = infinity; 
+	distances[sources[i]] = 0; 
+	//------------------
+    	fastest(sources[i]);
+    }
+    
+    print_avg_time();
+
+}
+
+//modified by sanaz
+void Graph::fastest(int source)
+{
+    Timer t;
+    t.start();
+		
+    /*define and initialize data structures*/	
+    vector<pair<int, int>> startPoints;
+    for(int i : Vout[source])
+	if(node_list[i].t >= t_start && node_list[i].t <= t_end)
+	  startPoints.push_back(make_pair(i, node_list[i].t)); 
+    sort(startPoints.begin( ), startPoints.end( ), [ ](const pair<int, int>& p1, const pair<int, int>& p2){
+       return p1.second > p2.second;
+    });
+    vector<bool> visited(node_list.size(), false);
+    queue<int> Q; 
+
+    for(auto it = startPoints.begin(); it != startPoints.end(); it++){
+	int ts = node_list[it->first].t;
+        visited[it->first] = true; 
+        Q.push(it->first);
+	while(!Q.empty()){
+	    int node = Q.front(); 
+	    Q.pop();
+	    for(auto neighbor=adj_list[node].begin(); neighbor!=adj_list[node].end(); neighbor++){
+		int nID = neighbor->first; 
+		Node neiNode = node_list[nID];
+		if(!visited[nID] && neiNode.t >= t_start && neiNode.t <= t_end){
+		   visited[nID] = true; 
+		   Q.push(nID);
+		   if(neiNode.isVin == true && (neiNode.t-ts) < distances[neiNode.u])
+		      distances[neiNode.u] = (neiNode.t-ts); 
+		}
+	    }
+	}
+     }
+	
+    t.stop();
+    time_sum += t.GetRuntime();
+
+    /*for debugging only*/
+    for(int i=0; i<distances.size(); i++)
+	cout << distances[i] << endl; 
+
+}
+//-----------------
+
+void Graph::run_shortest()
+{
+    time_sum=0;
+	
+    for(int i = 0 ;i < sources.size() ;i ++)
+    { 
+    	//modified by sanaz:
+	for(int j=0; j<V; j++)
+	  distances[j] = infinity; 
+	distances[sources[i]] = 0; 
+    	shortest(sources[i]);
+    }
+    
+    print_avg_time();
+
+}
+
+void Graph::shortest(int source)
+{
+    Timer t;
+    t.start();
+
+    /*defining and initializing data structures*/
+    typedef pair<int, int> iPair; 	
+    priority_queue< iPair, vector <iPair> , greater<iPair> > pq; //pairs of <distance, id> sorted in increasing order of distance
+    vector<int> local_dist(adj_list.size(), infinity); //distance vector for the nodes in the transformed graph
+    vector<bool> done(adj_list.size(), false); //keeping track of the nodes whose distance is established
+    
+    for(auto it=Vout[source].begin(); it!=Vout[source].end(); it++)
+	if(node_list[*it].t >= t_start && node_list[*it].t <= t_end){
+	   pq.push(make_pair(0, *it));
+	   local_dist[*it] = 0; 
+	} 
+
+    while(!pq.empty()){
+	int node = pq.top().second; 
+	pq.pop(); 
+	if(done[node]) 
+	   continue; 
+	done[node] = true; 
+	for(auto neigh=adj_list[node].begin(); neigh!=adj_list[node].end(); neigh++){
+	   int neiID = neigh->first;  
+	   if(!done[neiID] && node_list[neiID].t >= t_start && node_list[neiID].t <= t_end){
+		int newDist = local_dist[node]+neigh->second;
+		if(newDist < local_dist[neiID]){
+		   local_dist[neiID] = newDist; 
+		   pq.push(make_pair(newDist, neiID));  
+		}
+	   }
+  	}
+    }
+
+    for(int i=0; i<adj_list.size(); i++)
+	if(node_list[i].isVin && node_list[i].t >= t_start && node_list[i].t <= t_end){
+	   int u = node_list[i].u; 
+	   distances[u] = min(distances[u], local_dist[i]);
+	}
+	
+    t.stop();
+    time_sum += t.GetRuntime();
+
+    /*for debugging only*/
+    for(int i=0; i<distances.size(); i++)
+	cout << distances[i] << endl; 
+
+}
+
+void Graph::print_avg_time()
+{
+	cout<<"Average time: " << time_sum/numSources <<endl;
+}
