f7599ff5355f26c64860274a4ff8c87a47c2f46d
SanazGheibi <sgheibi@ufl.edu>
2021-03-31 21:40:15 -0400

    script added to produce the init files

---
+++ initScript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+./initProd 100 351 ./initFiles/init_100_airportJan2020.txt
+./initProd 100 28093 ./initFiles/init_100_arxiv.txt
+./initProd 100 116836 ./initFiles/init_100_conflict.txt
+./initProd 100 1824701 ./initFiles/init_100_dblp.txt
+./initProd 100 30360 ./initFiles/init_100_digg.txt
+./initProd 100 7115 ./initFiles/init_100_elec.txt
+./initProd 100 86978 ./initFiles/init_100_enron.txt
+./initProd 100 131580 ./initFiles/init_100_epin.txt
+./initProd 100 63731 ./initFiles/init_100_fb.txt
+./initProd 100 2302925 ./initFiles/init_100_flicker.txt
+./initProd 100 1870709 ./initFiles/init_100_growth.txt
+./initProd 100 51083 ./initFiles/init_100_slash.txt
+./initProd 100 3223585 ./initFiles/init_100_youtube.txt
